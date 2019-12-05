88b4a2b83dca6e1fa458eaa46193d3c34ea2a940
unknown <nickliu555@gmail.com>
2019-12-04 20:01:41 -0600

    Raw Data Scatter Plot

--- FinalPlotA/scatter_plot.js
+++ FinalPlotA/scatter_plot.js
@@ -1,4 +1,5 @@
-var object_clicked;
+var object_clicked = -1;
+var firstTime = true;
 
 function plot_it() {
     var svg_width = 2000; 
@@ -79,8 +80,8 @@ function plot_it() {
     // Display Title
     scatter_g.append('text').text('Spatial Error in VR')
 		.attr('transform', 'translate('+(graph_width/2)+','+(-graph_height-20)+')').attr('text-anchor', 'middle').attr('id', 'title').attr('font-size', '20px');
-	
-	// Function for selecting colors
+		
+    // Function for selecting colors
     var colorSelector = function(x)
         {
             if(x == 1)
@@ -134,7 +135,9 @@ function plot_it() {
 			
 	        return lineGenerator(points);
         })
-        .attr('fill', 'None').attr('stroke', d3.hsl(0, 1, 0)).attr('stroke-width', 1.5).attr('stroke-opacity', 0.33).attr('id', 'y_origin_line');
+        .attr('fill', 'None').attr('stroke', d3.hsl(0, 1, 0)).attr('stroke-width', 0).attr('stroke-opacity', 0.33).attr('id', 'y_origin_line')
+        .transition().duration(1200)
+        .attr('stroke-width', 1.5);
         
     scatter_g.append('path')
         .attr('stroke-opacity', 0)
@@ -147,7 +150,9 @@ function plot_it() {
 			
 	        return lineGenerator(points);
         })
-        .attr('fill', 'None').attr('stroke', d3.hsl(0, 1, 0)).attr('stroke-width', 1.5).attr('stroke-opacity', 0.33).attr('id', 'x_origin_line');
+        .attr('fill', 'None').attr('stroke', d3.hsl(0, 1, 0)).attr('stroke-width', 0).attr('stroke-opacity', 0.33).attr('id', 'x_origin_line')
+        .transition().duration(1200)
+        .attr('stroke-width', 1.5);
 		
      // Listen for when points are hovered
 	 scatter_g.selectAll('circle')
@@ -253,12 +258,256 @@ function plot_it() {
 	 // Listen for when points are clicked
 	 scatter_g.selectAll('circle')
 	    .on('click', function(d)  {
-	        object_clicked = -1;
+	        object_clicked = d.id;
+	        plot_it_b();
 	 });
 }
 
+
+var svg2;
+var scatter_g2;
+
 function plot_it_b() 
 {
-    var nick = all_data;
-   
+    // generates a line given an array [x, y]
+    var lineGenerator = d3.line()
+        .x(function(d, i) { return d[0] })
+        .y(function(d, i) { return d[1] });
+        
+    // Function for selecting colors
+    var colorSelector = function(x)
+        {
+            if(x == 1)
+                return d3.hsl(0, 1, 0.5, 0.33);
+            else if(x == 2)
+                return d3.hsl(35, 1, 0.5, 0.33);
+            else if(x == 3)
+                return d3.hsl(75, 1, 0.5, 0.33);
+            else if(x == 4)
+                return d3.hsl(120, 1, 0.5, 0.33);
+            else if(x == 5)
+                return d3.hsl(175, 1, 0.5, 0.33);
+            else if(x == 6)
+                return d3.hsl(240, 1, 0.5, 0.33);
+            else if(x == 7)
+                return d3.hsl(280, 1, 0.5, 0.33);
+            else if(x == 8)
+                return d3.hsl(315, 1, 0.5, 0.33);
+            else
+                return d3.hsl(0, 1, 0, 0.33);
+        }
+    
+    if(firstTime)
+    {
+        svg2 = d3.select("body").append("svg")
+            .attr("width", 2000)
+            .attr("height", 2000)
+            .append("g")
+            .attr("transform", "translate(" + 20 + "," + 10 + ")");
+        
+        scatter_g2 = svg2.append("g")
+            .attr("transform", "translate(" + 150 + "," + 450 + ")");
+    }
+
+    // Scatter plot dimensions
+    var graph_width = 400;
+    var graph_height = 400;
+ 
+    var errorData = [];
+    var minX = 1000;
+    var maxX = -1000;
+    var minY = 1000;
+    var maxY = -1000;
+    for(var i=0; i<all_data.length; i++)
+    {
+        if(all_data[i][' ObjectId'] == (object_clicked-1))
+        {
+            var currX = parseFloat(all_data[i][' ChangeErrorX']);
+            var currY = parseFloat(all_data[i][' ChangeErrorY']);
+        
+            // update min and max for X, if possible
+            if(currX > maxX)
+                maxX = currX;
+            else if(currX < minX)
+                minX = currX;
+                
+            // update min and max for Y, if possible
+            if(currY > maxY)
+                maxY = currY;
+            else if(currY < minY)
+                minY = currY;
+            
+            var obj = 
+            {
+                'x': currX,
+                'y': currY
+            };
+        
+            errorData.push(obj);
+        }
+    }
+    
+    // up/down scale (y-axis)
+    var ud_scale = d3.scaleLinear()
+        .domain([minY, maxY])
+        .range([graph_height, 0]);
+    
+    // left/right scale (x-axis)
+    var lr_scale = d3.scaleLinear()
+        .domain([minX, maxX])
+        .range([0, graph_width]);
+        
+    if(firstTime)
+    {
+        // Display Title
+        scatter_g2.append('text').text('Raw Data for Selected Object')
+		.attr('transform', 'translate('+(graph_width/2)+','+(-graph_height-20)+')').attr('text-anchor', 'middle').attr('id', 'title').attr('font-size', '20px');
+    
+        // Displaying scales (enter)
+	    scatter_g2.append('g').attr('transform', 'translate('+(0)+ ','+ (0)+')').attr('id', 'x_axis')
+	        .call(d3.axisBottom(lr_scale));
+    	
+	    scatter_g2.append('g').attr('transform', 'translate('+(0)+ ','+ (-graph_height)+')').attr('id', 'y_axis')
+	        .call(d3.axisLeft(ud_scale));
+	    
+	    // Display axis label
+        scatter_g2.append('text').text('Left/Right Error (in degrees)')
+		    .attr('transform', 'translate('+(graph_width/2)+','+(35)+')').attr('text-anchor', 'middle').attr('id', 'x_text');
+    		
+        scatter_g2.append('text').text('Down/Up Error (in degrees)')
+		    .attr('transform', 'translate('+(-30)+','+(-graph_height/2)+') rotate(270)').attr('text-anchor', 'middle').attr('id', 'y_text');
+		
+		// Generate line for (0,0)
+        scatter_g2.append('path')
+            .attr('stroke-opacity', 0)
+            .attr('d', function() {
+	            var points = [];
+	            // add the origin
+	            points.push([lr_scale(0), 0]);
+	            // add the current point location
+	            points.push([lr_scale(0), -graph_height]);
+    			
+	            return lineGenerator(points);
+            })
+            .attr('fill', 'None').attr('stroke', d3.hsl(0, 1, 0)).attr('stroke-width', 1.5).attr('stroke-opacity', 0.33).attr('id', 'y_origin_line');
+            
+        scatter_g2.append('path')
+            .attr('stroke-opacity', 0)
+            .attr('d', function() {
+	            var points = [];
+	            // add the origin
+	            points.push([0, ud_scale(0)-graph_height]);
+	            // add the current point location
+	            points.push([graph_width, ud_scale(0)-graph_height]);
+    			
+	            return lineGenerator(points);
+            })
+            .attr('fill', 'None').attr('stroke', d3.hsl(0, 1, 0)).attr('stroke-width', 1.5).attr('stroke-opacity', 0.33).attr('id', 'x_origin_line');
+	}
+	else
+	{   
+	    // Displaying scales (update)
+	    scatter_g2.selectAll('#x_axis').attr('transform', 'translate('+(0)+ ','+ (0)+')').attr('id', 'x_axis')
+	        .call(d3.axisBottom(lr_scale));
+    	
+	    scatter_g2.selectAll('#y_axis').attr('transform', 'translate('+(0)+ ','+ (-graph_height)+')').attr('id', 'y_axis')
+	        .call(d3.axisLeft(ud_scale));
+	        
+	    // Generate line for (0,0)
+        scatter_g2.selectAll('#y_origin_line')
+            .attr('stroke-opacity', 0)
+            .attr('d', function() {
+	            var points = [];
+	            // add the origin
+	            points.push([lr_scale(0), 0]);
+	            // add the current point location
+	            points.push([lr_scale(0), -graph_height]);
+    			
+	            return lineGenerator(points);
+            })
+            .attr('fill', 'None').attr('stroke', d3.hsl(0, 1, 0)).attr('stroke-width', 0).attr('stroke-opacity', 0.33).attr('id', 'y_origin_line')
+            .transition().duration(1200)
+            .attr('stroke-width', 1.5);
+            
+        scatter_g2.selectAll('#x_origin_line')
+            .attr('stroke-opacity', 0)
+            .attr('d', function() {
+	            var points = [];
+	            // add the origin
+	            points.push([0, ud_scale(0)-graph_height]);
+	            // add the current point location
+	            points.push([graph_width, ud_scale(0)-graph_height]);
+    			
+	            return lineGenerator(points);
+            })
+            .attr('fill', 'None').attr('stroke', d3.hsl(0, 1, 0)).attr('stroke-width', 0).attr('stroke-opacity', 0.33).attr('id', 'x_origin_line')
+            .transition().duration(1200)
+            .attr('stroke-width', 1.5);
+	}
+    
+    // Remove all points
+    scatter_g2.selectAll('circle').data([]).exit().remove();    
+	
+	// Display the scatter points
+    scatter_g2.selectAll('circle').data(errorData).enter().append('circle')
+        .attr('cx', function (d) {
+            var liu = d.x-0.000001;
+            var nick = lr_scale(d.x);
+            return lr_scale(d.x);
+        })
+        .attr('cy', function (d) { 
+            return ud_scale(d.y)-graph_height;
+        })
+        .attr('r', 0)
+        .attr('fill', function (d) {return colorSelector(object_clicked)})
+        .attr('id', 'points')
+        .transition().duration(1200)
+        .attr('r', 7);
+        
+     // Listen for when points are hovered
+	 scatter_g2.selectAll('circle')
+	    .on('mouseover', function(d)  {
+	    
+	        // Display the length value'
+	        var y_rounded = parseFloat(d.y).toFixed(2);
+	        var x_rounded = parseFloat(d.x).toFixed(2);
+		    var x_squared = d.x * d.x;
+		    var y_squared = d.y * d.y;
+		    // get the euclidian distance
+		    var result = Math.sqrt(x_squared + y_squared).toFixed(2);
+		    
+		    scatter_g2.append('text').text('('+x_rounded+', '+y_rounded+')')
+		        .attr('transform', 'translate('+(lr_scale(d.x))+','+(ud_scale(d.y)-graph_height-25)+')').attr('text-anchor', 'middle').attr('id', 'hoverText1').attr('font-size', '12px');
+		    
+		    scatter_g2.append('text').text('Length: '+result)
+		        .attr('transform', 'translate('+(lr_scale(d.x))+','+(ud_scale(d.y)-graph_height-10)+')').attr('text-anchor', 'middle').attr('id', 'hoverText2').attr('font-size', '12px');
+		   
+		   // Display the lines
+	       scatter_g2.append('path')
+                .attr('stroke-opacity', 0)
+		        .attr('d', function() {
+			        var points = [];
+			        // add the origin
+			        points.push([lr_scale(0), ud_scale(0)-graph_height]);
+			        // add the current point location
+			        points.push([lr_scale(d.x), ud_scale(d.y)-graph_height]);
+        			
+			        return lineGenerator(points);
+		        })
+		        .attr('fill', 'None').attr('stroke', function () {return colorSelector(d.id)}).attr('stroke-width', 2).attr('stroke-opacity', 0.5).attr('id', 'lineGraph');
+	    }); 
+	    
+	  // Listen for when points are unhovered
+      scatter_g2.selectAll('circle')
+        .on('mouseout', function(d)  {
+            // remove text
+            scatter_g2.selectAll('#hoverText1').data([]).exit().remove();
+            
+            scatter_g2.selectAll('#hoverText2').data([]).exit().remove();
+            
+            // remove line
+            scatter_g2.selectAll('#lineGraph').data([]).exit().remove();
+        });
+    
+    firstTime = false;
 }
